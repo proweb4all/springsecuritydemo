02cbb30ab50177aba2849078c68286f661edf2a7
proweb4all <proweb4all@narod.ru>
2020-12-21 16:56:28 +0300

    first commit

---
+++ src/main/java/ru/proweb4all/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,33 @@
+package ru.proweb4all.springsecuritydemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.proweb4all.springsecuritydemo.model.Developer;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List<Developer> developers = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+            new Developer(1L, "Petr", "Petrov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return developers.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
+
---
+++ src/test/java/ru/proweb4all/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.proweb4all.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
